b7ec6ed49b583bf9fd2086eba1b46bc6171eb632
John-LP <jonathanleplat@hotmail.fr>
2023-11-27 17:13:39 +0100

    ajout navbar, link vers login

--- resources/views/instabook/index.blade.php
+++ resources/views/instabook/index.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Accueil InstaBook')
 
 @section('content')
-    @foreach ($book as $item)
-        
-
-    @endforeach  
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga doloribus enim dolorem soluta quos, esse magni, minima totam saepe vel architecto pariatur consectetur incidunt nam deleniti aliquid. Culpa perferendis natus rem ipsa, magni maxime repellat ad ea modi, quas quae exercitationem fuga esse quos. Odit ut voluptatem minus quidem quaerat adipisci consequatur, cum nostrum dicta necessitatibus tempore fugiat cupiditate odio modi eligendi provident, unde illum temporibus corrupti amet neque? Blanditiis maiores delectus doloribus quas voluptatem ab, non unde. Maiores, deleniti distinctio quia sint fuga similique dolorem reprehenderit, reiciendis itaque dicta mollitia expedita iusto qui soluta. Earum, dolore facere! Culpa excepturi doloremque eum, commodi iste ullam. Libero deserunt facere, cumque nisi facilis commodi, tempora aperiam ipsam voluptatem neque deleniti, dolor voluptate?</p>
+    
+    {{-- @foreach ($book as $item)
+    @endforeach   --}}
 @endsection
