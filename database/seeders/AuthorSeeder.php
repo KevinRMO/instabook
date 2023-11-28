ecf31886c2a9905d0ebf17680318eac8ffbee264
John-LP <jonathanleplat@hotmail.fr>
2023-11-28 10:00:50 +0100

    Création seeder author

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -8,13 +8,30 @@ use Illuminate\Database\Seeder;
 
 class AuthorSeeder extends Seeder
 {
+    public $authors = [
+        ['firstname' => 'J.K.', 'lastname' => 'Rowling'],
+        ['firstname' => 'George', 'lastname' => 'Orwell'],
+        ['firstname' => 'Jane', 'lastname' => 'Austen'],
+        ['firstname' => 'Gabriel', 'lastname' => 'García Márquez'],
+        ['firstname' => 'Haruki', 'lastname' => 'Murakami'],
+        ['firstname' => 'Agatha', 'lastname' => 'Christie'],
+        ['firstname' => 'William', 'lastname' => 'Shakespeare'],
+        ['firstname' => 'J.R.R.', 'lastname' => 'Tolkien'],
+        ['firstname' => 'Toni', 'lastname' => 'Morrison'],
+        ['firstname' => 'Leo', 'lastname' => 'Tolstoy'],
+    ];
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        foreach ($this->liste as $author) {
-            Author::factory()->create(['author'=>$author]);
+        
+
+        foreach($this->authors as $author){
+            Author::factory()->create([
+                'firstname' => $author['firstname'],
+                'lastname' => $author['lastname'],
+            ]);
         }
     }
 }
