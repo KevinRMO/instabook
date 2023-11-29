c4e21f43ab3f06534758c43997874d4568021001
KevinRMO <romanokevin1992@gmail.com>
2023-11-29 17:09:38 +0100

    Creation fichier rateseeder(rate en alpha), modification carte show

---
+++ database/seeders/RateSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+// namespace Database\Seeders;
+
+// use App\Models\Rate;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Seeder;
+
+// class RateSeeder extends Seeder
+// {
+//     public $rates = [
+//     ['rate'=>1],
+//     ['rate'=>2],
+//     ['rate'=>3],
+//     ['rate'=>4],
+//     ['rate'=>5],
+//     ];
+//     /**
+//      * Run the database seeds.
+//      */
+//     public function run(): void
+//     {
+//         foreach ($this->rates as $rate) {
+//             Genre::factory()->create([
+//                 'rate'=>$rate['rate']
+//             ]);
+//         }
+//     }
+// }
--- resources/views/instabook/show.blade.php
+++ resources/views/instabook/show.blade.php
@@ -7,36 +7,30 @@
     <article>
         <section class="show">
             <div class="text-content">
-                <h2> {{$instabook->title}}</h2>
-                <h5>{{ $instabook->author->firstname }} {{ $instabook->author->lastname }}</h5>
-                <p>{{$instabook->year}} , {{$instabook->genre_id}}</p>
+                <h2>{{$instabook->title}}</h2><br>
+                <h3>{{ $instabook->author->firstname }} {{ $instabook->author->lastname }}</h3>
+                <p>{{$instabook->year}} , {{$instabook->genre->genre}}</p>
                 <p>{{$instabook->content}}</p>
+                {{-- <p>{{$instabook->rate}}</p> --}}
+                
+                <div class="form">  
+                    <form class="" action="{{route('instabook.edit', $instabook['id'])}}" method="get">
+                            @csrf
+                            <button class="editButton" type="submit"><i class="fa-solid fa-pencil"></i></button>                            
+                    </form>
+                    <form action="destroyButton"{{route('instabook.destroy', $instabook['id'])}} method="post">
+                        @csrf
+                        @method('delete')
+                        <button class="destroyButton" type="submit"><i class="fa-solid fa-trash "></i></button>
+                    </form>
+                </div>
             </div>
             <div class="visual">
                 <img
                     src="https://m.media-amazon.com/images/I/61HdYKaCHAL._SL1063_.jpg"
                     alt />
             </div>
-          
-
-            <br>
-            <div class="form">  
-                <form class="" action="{{route('instabook.edit', $instabook['id'])}}" method="get">
-                        @csrf
-                        <button class="editButton" type="submit"><i class="fa-solid fa-pencil"></i></button>
-                        
-                        </button>
-                    </form>
-                <form action="destroyButton"{{route('instabook.destroy', $instabook['id'])}} method="post">
-                    @csrf
-                    @method('delete')
-                    <button class="destroyButton" type="submit"><i class="fa-solid fa-trash "></i></button>
-                    
-                    </button>
-                </form>
-            </div>
-        </section>  
-
+        </section> 
     </article>
 @endsection
 
