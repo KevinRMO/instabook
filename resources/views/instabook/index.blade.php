fbd0e06747e6a9ac05416234dc16fbdc0eeb09a1
John-LP <jonathanleplat@hotmail.fr>
2023-11-29 21:47:24 +0100

    MàJ affichage principal Index, réduction des infos affichées pour les conserver pour show, miniature de la couverture du livre passée en affichage principal

--- resources/views/instabook/index.blade.php
+++ resources/views/instabook/index.blade.php
@@ -6,14 +6,12 @@
     @foreach ($instabook as $instabook)
         <article>
             <section class="card">
-                <div class="text-content">
-                    <h2>{{ $instabook->title }}</h2><br>
-                    <h3>{{ $instabook->author->firstname }} {{ $instabook->author->lastname }}</h3>
-                    <p>{{ $instabook->year }}, {{ $instabook->genre->genre }}</p>
-                    <p>{{ $instabook->content }}</p>
-                    <a href="{{route('instabook.show', $instabook->id)}}">En savoir plus</a>
-                </div>
-                    <img class="visual" src={{ $instabook->image_path }}>
+                <img class="visual" src={{ $instabook->image_path }}>
+                    <div class="text-content">
+                        <h2>{{ $instabook->title }}</h2><br>
+                        <h3>{{ $instabook->author->firstname }} {{ $instabook->author->lastname }}</h3>
+                        <a href="{{route('instabook.show', $instabook->id)}}">En savoir plus</a>
+                    </div>
                 <br>
             </section>
         </article>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,18 +11,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <script src="https://kit.fontawesome.com/f9d009cb2a.js" crossorigin="anonymous"></script>
-
         <!-- Scripts -->
         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
-
-
-    <body class="font-sans antialiased">
-
+    <body class="font-sans antialiased center-card">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
@@ -31,16 +25,10 @@
                     </div>
                 </header>
             @endif
-
             <!-- Page Content -->
             <main>
-
                 @include('layouts.navbar')
-                    {{-- @if (isset($slot))
-                        {{ $slot }}
-                    @endif --}}
                 @yield("content")
-
             </main>
         </div>
         <script src="/js/navbar.js"></script>
