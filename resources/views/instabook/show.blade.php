c2b96dc7ecc70bbc1cec7bf527022dc5ae07dd6c
KevinRMO <romanokevin1992@gmail.com>
2023-11-30 16:02:00 +0100

    permet de modifier ou supprimer les cards qu'on a créé

--- resources/views/instabook/show.blade.php
+++ resources/views/instabook/show.blade.php
@@ -12,18 +12,20 @@
                 <p>{{$instabook->year}} , {{$instabook->genre->genre}}</p>
                 <p>{{$instabook->content}}</p>
                 {{-- <p>{{$instabook->rate}}</p> --}}
-                <div class="form">  
-                    <form class="" action="{{route('instabook.edit', $instabook['id'])}}" method="get">
+                <div class="form">
+                    @if(auth()->check() && $instabook->user_id === auth()->user()->id)  
+                        <form class="" action="{{route('instabook.edit', $instabook['id'])}}" method="get">
+                                @csrf
+                                <button class="editButton" type="submit"><i class="fa-solid fa-pencil"></i></button>                            
+                        </form>
+                        <form action="{{ route('instabook.destroy', $instabook['id']) }}" method="post">
                             @csrf
-                            <button class="editButton" type="submit"><i class="fa-solid fa-pencil"></i></button>                            
-                    </form>
-                    <form action="{{ route('instabook.destroy', $instabook['id']) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button class="destroyButton" type="submit" onclick="return confirm('Voulez-vous vraiment supprimer ce livre ?')">
-                            <i class="fa-solid fa-trash "></i>
-                        </button>
-                    </form>
+                            @method('delete')
+                            <button class="destroyButton" type="submit" onclick="return confirm('Voulez-vous vraiment supprimer ce livre ?')">
+                                <i class="fa-solid fa-trash "></i>
+                            </button>
+                        </form>
+                    @endif
                 </div>
             </div>
             <img class="visual" src="/{{ $instabook->image_path }}">
