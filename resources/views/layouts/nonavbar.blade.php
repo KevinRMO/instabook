c0b5b01ff7a6d32976070d5d2ba69003524fb3b9
John-LP <jonathanleplat@hotmail.fr>
2023-11-30 10:35:59 +0100

    ajout d'une page de confirmation lors de la connexion avec message d'accueil et bouton pour accéder à l'index

---
+++ resources/views/layouts/nonavbar.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="/css/navbar.css" />
+        <link rel="stylesheet" href="/css/card.css" />
+        <link rel="stylesheet" href="/css/create.css">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/f9d009cb2a.js" crossorigin="anonymous"></script>
+        <!-- Scripts -->
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    </head>
+    <body class="font-sans antialiased center-card">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+            <!-- Page Content -->
+            <main>
+                @yield("content")
+            </main>
+        </div>
+        <script src="/js/navbar.js"></script>
+    </body>
+</html>
