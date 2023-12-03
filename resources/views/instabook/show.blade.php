929e59adeb3df38e129294e88d6959ac604a864c
KevinRMO <romanokevin1992@gmail.com>
2023-12-03 12:40:39 +0100

    Création du footer dans l'index/create/show

--- resources/views/instabook/show.blade.php
+++ resources/views/instabook/show.blade.php
@@ -45,4 +45,18 @@
             <img class="visual" src="/{{ $instabook->image_path }}">
         </section>  
     </article>
+        <footer>
+        <nav class="footerNavShow" >
+            <div class="logo" style="display: flex; align-items: center;">
+                <a href="/">
+                    <img src="https://i.ibb.co/x7jy2Sw/instabook-logo.png" alt="instabook-logo">
+                </a>
+            </div>
+            <ul class="nav-links">              
+                <li class="infoFooter"><p>Bocal Academy, 26 Bd Carabacel, 06000 Nice</p></li>
+                <li class="infoFooter"><p>04 ** ** ** **</p></li>
+                <li class="infoFooter"><p>©Copyright Melodie, Floriane, Jonathan, Kévin<p>
+            </ul>
+        </nav>
+    </footer>
 @endsection
